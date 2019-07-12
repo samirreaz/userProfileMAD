f235f4e2c7ebbeb16b85f943edabbbf3461a9b69
Mohammad shah samir bhuiyan reaz <mreaz151360@bscse.uiu.ac.bd>
2019-07-12 06:48:36 +0600

    User Profile

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.musicplayer;
+
+import android.Manifest;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Magnifier;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+    private Button start,stop;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //start = findViewById(R.id.btnStart);
+        //stop = findViewById(R.id.btnStop);
+
+        mediaPlayer= MediaPlayer.create(this,R.raw.xyz);
+        runTimePermission();
+        /*start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        //Toast t= (Toast) Toast.makeText(this,"complete", Toast.LENGTH_LONG);
+                    }
+                });
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+
+            }
+        });*/
+    }
+
+    public void runTimePermission(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
